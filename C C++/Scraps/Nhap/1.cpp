42672d4546eb520c4019c30da32bee6f6b508abe
cuongdztop1tg <caocuong090905@gmail.com>
2024-09-17 14:05:44 +0700

    Commit

--- C C++/Scraps/Nhap/1.cpp
+++ C C++/Scraps/Nhap/1.cpp
@@ -1,57 +1,27 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-const int inf = 1e9 + 7, maxN = 1e6 + 7;
-int n, a[maxN], minVal[4 * maxN];
-
-class segmentTree{
-    public:
-        void buildSegmentTree(int id, int l, int r){
-            if(l == r){
-                minVal[id] = a[l];
-                return;
-            }
-            buildSegmentTree(id * 2, l, (l + r) / 2);
-            buildSegmentTree(id * 2 + 1, (l + r) / 2 + 1, r);
-            minVal[id] = min(minVal[id * 2], minVal[id * 2 + 1]);
-        }
-
-        void update(int id, int l, int r, int i, int val){
-            if(l > i || r < i) return;
-            if(l == r){
-                minVal[id] = val;
-                return;
-            }
-            update(id * 2, l, (l + r) / 2, i, val);
-            update(id * 2 + 1, (l + r) / 2 + 1, r, i, val);
-            minVal[id] = min(minVal[id * 2], minVal[id * 2 + 1]);
-        }
-
-        int getMin(int id, int l, int r, int u, int v){
-            if(l > v || r < u) return inf;
-            if(l >= u && r <= v){
-                return minVal[id];
-            }
-            return min(getMin(id * 2, l, (l + r) / 2, u, v) , getMin(id * 2 + 1, (l + r) / 2 + 1, r, u, v));
-        }
-};
-
+const int maxN = 100001;
+long int n;
+int a[maxN];
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     cin >> n;
-    segmentTree tree;
     for(int i = 1; i <= n; i++){
         cin >> a[i];
     }
-    tree.buildSegmentTree(1, 1, n);
-    long long sum = 0;
-    int m;
-    cin >> m;
-    for(int i = 0; i < m; i++){
-        int u, v;
-        cin >> u >> v;
-        sum += tree.getMin(1, 1, n, u + 1, v + 1);
+    long int prefixSum[maxN];
+    prefixSum[0] = 0;
+    for(int i = 1; i <= n; i++){
+        prefixSum[i] = prefixSum[i - 1] + a[i];
     }
-    cout << sum;
+    int Q;
+    cin >> Q;
+    for(int k = 0; k < Q; k++){
+        int i, j;
+        cin >> i >> j;
+        cout << prefixSum[j] - prefixSum[i - 1];
+    }
     return 0;
 }
