b1103597ca0b04f7b8208b01339b718b37139ad8
cuongdztop1tg <caocuong090905@gmail.com>
2024-09-17 16:31:30 +0700

    Commit

---
+++ C C++/Hustack/AppAlgos/Week2/cummulative2DArray.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n, m;
+    cin >> n >> m;
+    int a[n + 1][m + 1] = {0};
+    long int Q;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            cin >> a[i][j];
+        }
+    }
+
+    long int prefixSum[n + 1][m + 1];
+    for(int i = 0; i <= n; i++){
+    	prefixSum[i][0] = 0;
+	}
+	for(int i = 0; i <= m; i++){
+    	prefixSum[0][i] = 0;
+	}
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            prefixSum[i][j] = prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1] + a[i][j];
+        }
+    }
+	
+    cin >> Q;
+    for(int i = 0; i < Q; i++){
+        int r1, c1, r2, c2;
+        cin >> r1 >> c1 >> r2 >> c2;
+        cout << prefixSum[r2][c2] - prefixSum[r1 - 1][c2] - prefixSum[r2][c1 - 1] + prefixSum[r1 - 1][c1 - 1] << endl;
+    }
+    return 0;
+}
--- C C++/Scraps/Nhap/1.cpp
+++ C C++/Scraps/Nhap/1.cpp
@@ -1,27 +1,46 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-const int maxN = 100001;
-long int n;
-int a[maxN];
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cin >> n;
+    
+    int n, m;
+    cin >> n >> m;
+    int a[n + 1][m + 1] = {0};
+    long int Q;
     for(int i = 1; i <= n; i++){
-        cin >> a[i];
+        for(int j = 1; j <= m; j++){
+            cin >> a[i][j];
+        }
     }
-    long int prefixSum[maxN];
-    prefixSum[0] = 0;
+    cout << endl;
+    long int prefixSum[n + 1][m + 1];
+    for(int i = 0; i <= n; i++){
+    	prefixSum[i][0] = 0;
+	}
+	for(int i = 0; i <= m; i++){
+    	prefixSum[0][i] = 0;
+	}
     for(int i = 1; i <= n; i++){
-        prefixSum[i] = prefixSum[i - 1] + a[i];
+        for(int j = 1; j <= m; j++){
+            prefixSum[i][j] = prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1] + a[i][j];
+        }
     }
-    int Q;
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= m; j++){
+			cout << prefixSum[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	
     cin >> Q;
-    for(int k = 0; k < Q; k++){
-        int i, j;
-        cin >> i >> j;
-        cout << prefixSum[j] - prefixSum[i - 1];
+    for(int i = 0; i < Q; i++){
+        int r1, c1, r2, c2;
+        cin >> r1 >> c1 >> r2 >> c2;
+        cout << prefixSum[r2][c2] - prefixSum[r1 - 1][c2] - prefixSum[r2][c1 - 1] + prefixSum[r1 - 1][c1 - 1] << endl;
     }
     return 0;
 }
