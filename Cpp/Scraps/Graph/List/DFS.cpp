063002b035627289949bea51b924e858671361bc
cuongdztop1tg <caocuong090905@gmail.com>
2024-09-29 18:29:52 +0700

    Commit

--- Cpp/Scraps/Graph/List/DFS.cpp
+++ Cpp/Scraps/Graph/List/DFS.cpp
@@ -2,18 +2,22 @@
 
 using namespace std;
 
-int V;
+int V, Count = 0, Time = 0;
 vector<vector<int>> graph;
 vector<bool> visited;
-vector<int> d;
-vector<int> pred;
+vector<int> d, f, pred, id;
 
 void DFS(int s){
     visited[s] = true;
     cout << s << " ";
+    Time++;
+    d[s] = Time;
+    id[s] = Count;
     for(auto i : graph[s]){
         if(!visited[i]) DFS(i);
     }
+    Time++;
+    f[s] = Time;
 }
 
 int main(){
@@ -21,15 +25,38 @@ int main(){
     graph.resize(V);
     visited.resize(V, false);
     d.resize(V);
+    f.resize(V);
     pred.resize(V);
+    id.resize(V);
     while(1){
         int initial, terminal;
         cin >> initial;
         if(initial == -1) break;
         cin >> terminal;
         graph[initial].push_back(terminal);
-        graph[terminal].push_back(initial);
+        //graph[terminal].push_back(initial);
     }
-    DFS(0);
+    
+    for(int i = 0; i < V; i++){
+        if(!visited[i]){
+            Count++;
+            cout << "Source vertex " << i << ": ";
+            DFS(i);
+            cout << endl;
+        }
+    }
+
+    for(int i = 0; i < V; i++){
+        cout << "Vertex " << i << ": " << d[i] << " " << f[i] << endl; 
+    }
+
+    cout << "Number of connected components: " << Count << endl;
+    for(int i = 1; i <= Count; i++){
+        cout << "Component " << i << ": ";
+        for(int j = 0; j < V; j++){
+            if(id[j] == i) cout << j << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
