bf2ef2d36e9d8944690e71b98db38e75e2167e89
cuongdztop1tg <caocuong090905@gmail.com>
2024-10-15 11:09:59 +0700

    Commit

---
+++ JavaPractice/PracticeWithPackage/Student/Student.java
@@ -0,0 +1,16 @@
+package JavaPractice.PracticeWithPackage.Student;
+
+public class Student{
+    private String name;
+    private int year;
+    public Student(String name, int year){
+        this.name = name;
+        this.year = year;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getYear(){
+        return this.year;
+    }
+}
