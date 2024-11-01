d316c7b72406c61a4c9033ded0dad544d9419639
cuongdztop1tg <caocuong090905@gmail.com>
2024-11-01 10:50:10 +0700

    Commit

---
+++ RARS/MidTerm/ex4.asm
@@ -0,0 +1,129 @@
+.data 
+	X:	.space 400	#array to store the elements
+	message1:	.asciz "Enter array size: "
+	message2:	.asciz "Enter array: "
+	message3:	.asciz "Max element of the array: "
+	message4:	.asciz "Enter m: "
+	message5:	.asciz "Enter M: "
+	message6:	.asciz "Number of elements in range (m, M): "
+.text
+#Workflow
+#--------------------------------------------------------------
+# Step 1: Input the size of array and the elements of the array
+# Step 2: Find the max element of the array
+# Step 3: Find the elements in the range (m, M)
+#--------------------------------------------------------------
+
+#Input the size of array and the elements of the array
+input:
+	#Enter array size
+	li	a7, 4
+	la	a0, message1
+	ecall
+	#read array size
+	li	a7, 5
+	ecall
+	
+	add	s0, a0, zero
+	#print message2
+	li	a7, 4
+	la	a0, message2
+	ecall
+	
+	li	t0, 0	#t0 is the index i of the array X, t0 = i = 0
+	la	s1, X	#set address of X to s1
+
+	#loop to input the array
+input_loop:
+	slt	t2, t0, s0	#check loop condition i < size
+	beq	t2, zero, end_input_loop	#end loop if i >= size
+	li	a7, 5		#input element and store in a0
+	ecall
+	sw	a0, 0(s1)	#X[i] = a0
+	addi	s1, s1, 4	#move to next element
+	addi	t0, t0, 1	#i = i + 1
+	j	input_loop
+end_input_loop:
+end_input:
+
+#Find max function: find the maximum element of X
+#---------------------------------------------------
+# Set the value of max to be X[0]
+# Iterate through the array
+# if X[i] > max, set max = X[i]
+# print max
+#---------------------------------------------------
+find_max:
+	la	s1, X	#set s1 to be the base address of X
+	lw	s2, 0(s1)	#s2 store the max element of X
+				#set s2 = X[0]
+	li	t0, 1	#i = 1
+find_max_loop:
+	slt	t2, t0, s0	#check loop condition i < size
+	beq	t2, zero, end_find_max_loop	#end loop if i >= size
+	
+	addi	s1, s1, 4	#move to next element
+	lw	t3, 0(s1)	#t4 = X[i]
+	ble	t3, s2, continue	#if t3 <= max, continue
+	add	s2, t3, zero	#else, update max = t3
+continue:
+	addi	t0, t0, 1	#i = i + 1
+	j	find_max_loop
+end_find_max_loop:
+	#print the result
+	li 	a7, 56
+	la	a0, message3
+	add	a1, s2, zero
+	ecall
+end_find_max:
+
+#Find the number of element in range (m, M)
+#---------------------------------------------------
+# Read input m, M
+# initialize count = 0
+# Iterate through the array
+# If X[i] in the range (m, M), count++
+#---------------------------------------------------
+num_elements_in_range:
+	#Enter m
+	li	a7, 4
+	la	a0, message4
+	ecall
+	#read m
+	li	a7, 5
+	ecall
+	add	a4, a0, zero	#store m to a4
+	
+	#Enter M
+	li	a7, 4
+	la	a0, message5
+	ecall
+	#read M
+	li	a7, 5
+	ecall
+	add	a5, a0, zero	#store M to a5
+	
+	la	s1, X	#set s1 to be the base address of X
+	li	t0, 0	#i = 0
+	li	t5, 0	#count = 0
+num_elements_in_range_loop:
+	slt	t2, t0, s0	#check loop condition i < size
+	beq	t2, zero, end_num_elements_in_range_loop	#end loop if i >= size
+	
+	lw	t3, 0(s1)
+	blt	t3, a5, check_larger_than_m	#if X[i] < M, then
+	j	cont
+check_larger_than_m:
+	ble	t3, a4, cont	#if X[i] <= m => skip
+	addi	t5, t5, 1	#else, count += 1
+cont:
+	addi	s1, s1, 4	#move to next element
+	addi	t0, t0, 1	#i = i + 1
+	j	num_elements_in_range_loop
+end_num_elements_in_range_loop:
+	#print the result
+	li 	a7, 56
+	la	a0, message6
+	add	a1, t5, zero
+	ecall
+end_num_elements_in_range:
---
+++ RARS/MidTerm/ex8.asm
@@ -0,0 +1,97 @@
+.data
+	names:	.space 3200	#array to store students' names
+				#max name length = 32
+	marks:	.space 400	#array to store students' marks
+
+	message1	:	.asciz "Enter number of students: "
+	message2:	.asciz "Enter list of students: "
+.text
+#Workflow
+#-------------------------------------------------------------
+# Step 1: Input the size of 2 arrays and element of each array
+# Step 2: Sort students base on their marks
+#-------------------------------------------------------------
+
+#Input function: Enter the number of students and read the name
+#and mark of each students
+input:
+	#Enter number of students
+	li	a7, 4
+	la	a0, message1
+	ecall
+	#Read number of students 
+	li	a7, 5
+	ecall
+	
+	add	s0, a0, zero	#store the number of students in s0
+	#Enter list of students
+	li	a7, 4
+	la	a0, message2
+	ecall
+	
+	li	t0, 0		#index of 2 arrays, i = 0
+	la	s1, names	#ste address of names to s1
+	la	s2, marks	#set address of marks to s2
+input_loop:
+	slt	t2, t0, s0	#check loop condition i < size
+	beq	t2, zero, end_input_loop	#end loop if i >= size
+	
+	#read name and store to s1
+	li	a7, 8
+	add	a0, s1, zero
+	li	a1, 32
+	ecall
+	addi	s1, s1, 32	#move to next element
+	
+	#read mark
+	li	a7, 7
+	ecall
+	sw	a0, 0(s2)	#store input mark in s2
+	addi	s2, s2, 4	#move to next element
+	
+	addi	t0, t0, 1	#i = i + 1
+	j	input_loop
+end_input_loop:		
+end_input:
+
+#Sort students base on their marks
+
+
+
+#print list of students
+print:
+	li	t0, 0
+	la	s1, names
+	la	s2, marks
+print_loop:
+	slt	t2, t0, s0	#check loop condition i < size
+	beq	t2, zero, end_print_loop	#end loop if i >= size
+	
+	#print name
+	li	a7, 4
+	add	a0, s1, zero
+	ecall
+	addi	s1, s1, 32	#move to next element
+
+	#print mark
+	li	a7, 3
+	lw	a0, 0(s2)
+	ecall
+	addi	s2, s2, 4	#move to next element
+	
+	#new line
+	li	a7, 11
+	li	a0, 10
+	ecall
+		
+	addi	t0, t0, 1
+	j	print_loop
+end_print_loop:
+
+
+
+
+
+
+
+
