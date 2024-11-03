25b1d5660b96433f832def0f9f9425d59759a057
cuongdztop1tg <caocuong090905@gmail.com>
2024-11-03 22:41:28 +0700

    Commit

--- Cpp/Scraps/AppAlgosFinal20201/Ex2.cpp
+++ Cpp/Scraps/AppAlgosFinal20201/Ex2.cpp
@@ -2,9 +2,7 @@
 
 using namespace std;
 
-int LIS(vector<long>& a){
-    
-}
+const int N = 1e5 + 5;
 
 int main(){
     int T;
@@ -12,11 +10,16 @@ int main(){
     while(T--){
         int n;
         cin >> n;
-        vector<long> a(n + 1);
+        long a[N], dp[N];
         for(int i = 1; i <= n; i++){
             cin >> a[i];
+            dp[a[i]] = 0;
         }
-        cout << LIS(a) << endl;
+        int maxLIS = 0;
+        for(int i = 1; i <= n; i++){
+            dp[a[i]] = 1 + dp[a[i] - 1];
+        }
+        cout << *max_element(dp + 1, dp + n + 1) << endl;
     }
     return 0;
 }
