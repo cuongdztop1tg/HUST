a2dc64b79f5c3918c39d68900f811a450f44baba
cuongdztop1tg <caocuong090905@gmail.com>
2024-11-04 01:00:40 +0700

    Commit

--- Cpp/Scraps/AppAlgosFinal20201/Ex3.cpp
+++ Cpp/Scraps/AppAlgosFinal20201/Ex3.cpp
@@ -1,34 +1,31 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+const int N = 1e5 + 5;
 int main(){
     int T;
     cin >> T;
     while(T--){
         int n;
         cin >> n;
-        vector<int> a(n);
+        int a[N];
         for(int i = 0; i < n; i++){
             cin >> a[i];
         }
         long res = 0;
         int k = 1;
-        int size = a.size();
+        int size = n;
         while(size > 0){
             int time = 0;
             int step = 2 * k;
-            for(int i = 0; i < a.size(); i += step){
+            for(int i = 0; i < n; i += step){
+                if(k + i > n) break;
                 a[i] += a[k + i];
                 if(a[i] > time) time = a[i];
                 size--;
             }
-            for(int i = 0; i < a.size(); i++){
-                cout << a[i] << " ";
-            }
-            cout << endl;
             res += time;
-            k++;
+            k *= 2;
         }
         cout << res << endl;
     }
