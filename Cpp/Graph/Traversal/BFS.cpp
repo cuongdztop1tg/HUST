2b1aa3a74bea0a0a307b4797d6e12ed6991f49f1
cuongdztop1tg <caocuong090905@gmail.com>
2024-11-25 19:01:50 +0700

    BFS

--- Cpp/Graph/Traversal/BFS.cpp
+++ Cpp/Graph/Traversal/BFS.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 int V, E;
+//Adjacency list, directed graph
+vector<vector<int>> graph;
+//Store the distance from source to another vertex
+vector<int> dis;
+//pred[v] = u --> u preceding v
+vector<int> pred;
 
 void BFS(vector<vector<int>>& graph, int source){
     queue<int> q;
@@ -10,12 +16,14 @@ void BFS(vector<vector<int>>& graph, int source){
     //Push source into the queue
     q.push(source);
     visited[source] = true;
+    dis[source] = 0;
     while(!q.empty()){
         int node = q.front();
         cout << node << " ";
         for(int neighbour : graph[node]){
             if(!visited[neighbour]){
                 q.push(neighbour);
+                dis[neighbour] = dis[node] + 1;
                 visited[neighbour] = true;
             }
         }
@@ -24,8 +32,9 @@ void BFS(vector<vector<int>>& graph, int source){
 
 int main(){
     cin >> V >> E;
-    //Adjacency list, directed graph
-    vector<vector<int>> graph(V);
+    graph.resize(V);
+    pred.resize(V, -1);
+    dis.resize(V, INT_MAX);
     //Create graph
     for(int i = 0; i < E; i++){
         int initial, terminal;
