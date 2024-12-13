90c80d28dbf812d972797380f8750179eaff26bf
cuongdztop1tg <caocuong090905@gmail.com>
2024-12-13 22:41:07 +0700

    Nháp thôi mà

--- Cpp/Scraps/Nhap/1.cpp
+++ Cpp/Scraps/Nhap/1.cpp
@@ -1,19 +1,65 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main(){
-    int V, E;
-    cin >> V >> E;
-    vector<int> deg(V + 1);
-    for(int i = 0; i < E; i++){
-        int x, y;
-        cin >> x >> y;
-        deg[x]++;
-        deg[y]++;
+const int MAXN = 1003;
+vector<pair<int, int>> capG[MAXN];
+int pred[MAXN];
+int V, E, s, t;
+
+bool bfs(int s, int t) {
+    memset(pred, -1, sizeof(pred));
+    queue<int> q;
+    q.push(s);
+    pred[s] = s;
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for (auto v : capG[u]) {
+            if (pred[v.first] == -1 && v.second != 0) {
+                pred[v.first] = u;
+                if (v.first == t) return true;
+                q.push(v.first);
+            }
+        }
     }
-    for(int i = 1; i <= V; i++){
-        if(i % 2 != 0) cout << deg[i] << " ";
+    return false;
+}
+
+pair<int, int> *findEdge(int u, int v) {
+    for (pair<int, int> &edge : capG[u]) {
+        if (edge.first == v) {
+            return &edge;
+        }
     }
+    return nullptr;
+}
+
+int Edmond_Karp(int s, int t) {
+    int maxflow = 0;
+    while (bfs(s, t)) {
+        int capP = INT_MAX;
+        for (int v = t; v != s; v = pred[v]) {
+            int u = pred[v];
+            capP = min(capP, findEdge(u, v)->second);
+        }
+        for (int v = t; v != s; v = pred[v]) {
+            int u = pred[v];
+            findEdge(u, v)->second -= capP;
+            findEdge(v, u)->second += capP;
+        }
+        maxflow += capP;
+    }
+    return maxflow;
+}
+
+int main() {
+    cin >> V >> E >> s >> t;
+    for (int i = 0; i < E; i++) {
+        int x, y, w;
+        cin >> x >> y >> w;
+        capG[x].push_back({y, w});
+        capG[y].push_back({x, 0});
+    }
+    cout << Edmond_Karp(s, t) << endl;
     return 0;
 }
--- Cpp/Scraps/Nhap/3.cpp
+++ Cpp/Scraps/Nhap/3.cpp
@@ -2,91 +2,27 @@
 
 using namespace std;
 
-int V, m, MSTweight = 0;
-int coordinate[750][2];
-vector<pair<int, pair<int, int>>> graph, MST;   //{u, {v, weight}}
-
-//Union Find
-vector<int> parent, sz;
-void makeSet(){
-    parent.resize(V + 1);
-    sz.resize(V + 1);
-    for(int i = 1; i <= V; i++){
-        parent[i] = i;
-        sz[i] = 1;
+int fibo(int n){
+    if(n <= 1) return n;
+    int i = 0, j = 1;
+    for(int k = 2; k <= n; k++){
+        int temp = j;
+        j = j + i;
+        i = temp;
     }
+    return i;
 }
 
-int Find(int u){
-    if(u == parent[u]) return u;
-    else return parent[u] = Find(parent[u]);
-}
-
-bool Union(int u, int v){
-    u = Find(u);
-    v = Find(v);
-    if(u == v) return false;
-    if(sz[u] < sz[v]){
-        swap(u, v);
-    }
-    sz[u] += sz[v];
-    parent[v] = u;
-    return true;
-}
-//custom comp function for sorting
-//compare the weight
-bool comp(const pair<int, pair<int, int>> &a, const pair<int, pair<int, int>> &b){
-    return a.second.second < b.second.second;
-}
-
-int calWeight(int Ax, int Bx, int Ay, int By){
-    return (Ax - Bx)*(Ax - Bx) - (Ay - By)*(Ay - By);
-}
-
-void Kruskal(){
-    sort(graph.begin(), graph.end(), comp); //sort edges in ascending order by their weights
-    //Iterate through all the edges
-    for(auto edge : graph){
-        //check if the 2 vertices belongs to the same set
-        //If true, adding the edge creates a cycle
-        int rootU = Find(edge.first);
-        int rootV = Find(edge.second.first);
-        //Else, add the edge to T
-        if(rootU != rootV){
-            MST.push_back(edge);
-            MSTweight += edge.second.second;
-            Union(rootU, rootV);
-            if(MST.size() == V - 1){
-                cout << MSTweight << endl;
-            }
-        }
-    }
-}
-//kruskal
 int main(){
-    makeSet();
-    cin >> V;
-    //coordinate
-    for(int i = 1; i <= V; i++){
-        int x, y;
-        cin >> x >> y;
-        coordinate[i][0] = x;
-        coordinate[i][1] = y;
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        cout << fibo(n) << endl;
     }
-    //build edge
-    for(int i = 1; i <= V; i++){
-        for(int j = i + 1; j <= V; j++){
-            int w = calWeight(coordinate[i][0], coordinate[i][1], coordinate[j][0], coordinate[j][1]);
-            graph.push_back({i, {w, j}});
-            graph.push_back({j, {w, i}});
-        }
-    }
-    cin >> m;
-    for(int i = 0; i < m; i++){
-        int x, y;
-        cin >> x >> y;
-        Union(x, y);
-    }
-    Kruskal();
     return 0;
 }
