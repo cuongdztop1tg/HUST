2e68348cab57b3088e91c146c765d63761389875
cuongdztop1tg <caocuong090905@gmail.com>
2024-12-13 22:41:16 +0700

    Do later

---
+++ Cpp/Hustack/AppAlgosLAB/MaxEvenSubseq.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a[100001];
+int n;
+
+void input(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+}
+
+void solve(){
+    
+}
+
+int main(){
+    input();
+    return 0;
+}
