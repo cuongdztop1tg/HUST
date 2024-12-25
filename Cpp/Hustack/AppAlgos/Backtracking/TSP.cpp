457b54fc4875cd0a527757ff9dc5896c9d9e09ab
cuongdztop1tg <caocuong090905@gmail.com>
2024-12-25 23:55:47 +0700

    Thêm path

--- Cpp/Hustack/AppAlgos/Backtracking/TSP.cpp
+++ Cpp/Hustack/AppAlgos/Backtracking/TSP.cpp
@@ -6,6 +6,7 @@ int n, opt = INT_MAX, cur = 0, minDistance = INT_MAX, total;
 vector<vector<int>> d;
 vector<bool> visited;
 vector<int> city;
+vector<int> path;
 
 void TSP(int k){
     for(int i = 2; i <= n; i++){
@@ -15,7 +16,15 @@ void TSP(int k){
             cur += d[city[k - 1]][city[k]];
             if(k == n){
                 total = cur + d[city[n]][city[1]];
-                if(total < opt) opt = total;
+                if(total < opt){
+                    opt = total;
+                    for(int i = 1; i <= n; i++){
+                        path[i] = city[i];
+                    }
+                    cout << opt << endl;
+                    for(int i = 1; i <= n; i++) cout << path[i] << " ";
+                    cout << endl;
+                }
             }
             else{
                 if(cur + (n - k + 1)*minDistance < opt) TSP(k + 1);
@@ -31,14 +40,16 @@ int main(){
     d.resize(n + 1, vector<int> (n + 1));
     visited.resize(n + 1, false);
     city.resize(n + 1);
+    path.resize(n + 1);
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= n; j++){
             cin >> d[i][j];
-            if(d[i][j] < minDistance) minDistance = d[i][j];
+            if(i != j && d[i][j] < minDistance) minDistance = d[i][j];
         }
     }
     city[1] = 1;
     TSP(2);
-    cout << opt << endl;
+    // cout << opt << endl;
+    // for(int i = 1; i <= n; i++) cout << path[i] << " ";
     return 0;
 }
