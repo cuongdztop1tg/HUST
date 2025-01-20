c675cd84c1c633c2280bb6182e04c3654ee497b4
cuongdztop1tg <caocuong090905@gmail.com>
2025-01-20 20:32:27 +0700

    super ez EK

--- Cpp/Graph/Max flow/EdmondsKarp.cpp
+++ Cpp/Graph/Max flow/EdmondsKarp.cpp
@@ -2,23 +2,23 @@
 
 using namespace std;
 
-const int MAXN = 1000;
-int capG[MAXN][MAXN], capRG[MAXN][MAXN];
-int pred[MAXN];
-int n, m, s, t;
+int V, E;
+vector<vector<int>> capG;   //original graph, adj matrix
+vector<vector<int>> capRG;  //residual graph, adj matrix
+vector<int> pred;
 
-bool bfs(int s, int t) {
-    memset(pred, -1, sizeof(pred));
+bool BFS(int s, int t){
+    fill(pred.begin(), pred.end(), -1);
     queue<int> q;
     q.push(s);
     pred[s] = s;
-    while (!q.empty()) {
+    while(!q.empty()){
         int u = q.front();
         q.pop();
-        for (int v = 0; v < n; v++) {
-            if (pred[v] == -1 && capRG[u][v] > 0) {
+        if(u == t) return true;
+        for(int v = 1; v <= V; v++){
+            if(pred[v] == -1 && capRG[u][v] > 0){
                 pred[v] = u;
-                if (v == t) return true;
                 q.push(v);
             }
         }
@@ -26,47 +26,36 @@ bool bfs(int s, int t) {
     return false;
 }
 
-int EK(int s, int t) {
-    memcpy(capRG, capG, sizeof(capG));
-    int maxflow = 0;
-    while (bfs(s, t)) {
+int EK(int s, int t){
+    int maxFlow = 0;
+    while(BFS(s, t)){
         int capP = INT_MAX;
-        for (int v = t; v != s; v = pred[v]) {
+        for(int v = t; v != s; v = pred[v]){
             int u = pred[v];
             capP = min(capP, capRG[u][v]);
         }
-        for (int v = t; v != s; v = pred[v]) {
+        for(int v = t; v != s; v = pred[v]){
             int u = pred[v];
             capRG[u][v] -= capP;
             capRG[v][u] += capP;
         }
-        maxflow += capP;
+        maxFlow += capP;
     }
-    return maxflow;
+    return maxFlow;
 }
-
-void input() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin >> n >> m >> s >> t;
-
-    // Convert to 0-indexed
-    s--; t--;
-
-    memset(capG, 0, sizeof(capG));
-    for (int i = 0; i < m; i++) {
-        int x, y, w;
-        cin >> x >> y >> w;
-        // Convert to 0-indexed
-        x--; y--;
-        capG[x][y] = w;
+int main(){
+    int s, t;
+    cin >> V >> E >> s >> t;
+    capG.resize(V + 1, vector<int>(V + 1, 0));
+    capRG.resize(V + 1, vector<int>(V + 1, 0));
+    pred.resize(V + 1, -1);
+    for(int i = 0; i < E; i++){
+        int u, v, w;
+        cin >> u >> v >> w;
+        capG[u][v] = w;
+        capRG[u][v] = w;
     }
-    cout << EK(s, t) << endl;
-}
-
-int main() {
-    input();
+    cout << EK(s, t);
     return 0;
 }
 
